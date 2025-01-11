b78a863c2f3eda71b1df8d35539d1ca37ff743bd
NourJadiri <ick62377@gmail.com>
2025-01-11 17:55:41 +0100

    Write unit tests for chadd_scraper

--- airflow/dags/src/test_chadd_scraper.py
+++ airflow/dags/src/test_chadd_scraper.py
@@ -2,7 +2,6 @@
 import os
 from dotenv import load_dotenv
 
-from chadd.models.post import Post
 from chadd_scrap import ChaddScraper
 
 load_dotenv()
@@ -16,14 +15,26 @@ chadd_scraper = ChaddScraper(
     base_url = 'https://healthunlocked.com/'
 )
 
-chadd_scraper.login()
-
-postIds = chadd_scraper.get_posts_ids(start_date='2020-01', end_date='2020-02', community='adult-adhd')
-
 posts = []
 
-for post_id in postIds:
-    post_details = chadd_scraper.get_post_details(post_id, community='adult-adhd')
-    new_post = Post.from_json(post_details)
-    print(new_post.title)
+def test_chadd_scraper_login():
+    chadd_scraper.login()
+
+    assert chadd_scraper.huSessID is not None
+    assert chadd_scraper.huBv is not None
+
+
+def test_get_posts_ids():
+    post_ids = chadd_scraper.get_posts_ids(start_date='2020-01', end_date='2020-02', community='adult-adhd')
+    assert len(post_ids) > 0
+    assert all(isinstance(post_id, int) for post_id in post_ids)
+
+
+def test_get_post_details():
+    post_id = 151579120
+    post = chadd_scraper.get_post_details(post_id)
+    assert isinstance(post, dict)
+    assert post.get('id') == post_id
+
+
 
